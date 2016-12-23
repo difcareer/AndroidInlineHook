6990b942888b8bac9067eb74cf3c302baeb77365
wangwanchun <wangwanchun@mobimagic.com>
2016-12-23 13:04:26 +0800

    add

---
+++ app/src/main/java/com/andr0day/androidinlinehook/MainActivity.java
@@ -0,0 +1,33 @@
+package com.andr0day.androidinlinehook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stringFromJNI();
+            }
+        });
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
---
+++ app/src/main/jni/native-lib.c
@@ -0,0 +1,44 @@
+#include <jni.h>
+#include "inlineHook.h"
+#include "android_log.h"
+
+void (*old_log)(const char *) = NULL;
+
+void origin_log(const char *str) {
+    LOGE("%s", str);
+}
+
+void new_log(const char *str) {
+    old_log("log hooked");
+}
+
+int hook() {
+    if (registerInlineHook((uint32_t) origin_log, (uint32_t) new_log, (uint32_t **) &old_log)
+        != ELE7EN_OK) {
+        return -1;
+    }
+    if (inlineHook((uint32_t) puts) != ELE7EN_OK) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int unHook() {
+    if (inlineUnHook((uint32_t) puts) != ELE7EN_OK) {
+        return -1;
+    }
+
+    return 0;
+}
+
+
+jstring
+Java_com_andr0day_androidinlinehook_MainActivity_stringFromJNI(JNIEnv *env, jobject obj) {
+    origin_log("test");
+    hook();
+    origin_log("test");
+    unHook();
+    origin_log("test");
+    return (*env)->NewStringUTF(env, "WTF");
+}
